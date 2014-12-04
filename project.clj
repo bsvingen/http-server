a845fad25928aaefbae3225fceb73391233fbe21
Børge Svingen <bsvingen@borkdal.com>
2014-12-04 19:46:41 +0100

    Adding metadata to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject com.borkdal/http-server "0.1.0-SNAPSHOT"
+  :description "Simple HTTP server, serving files from local disk."
+  :url "https://github.com/bsvingen/http-server"
+  :license {:name "Apache License 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [http-kit "2.1.16"]
                  [com.novemberain/pantomime "2.3.0"]]
